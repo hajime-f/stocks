3876c0ef1091bcb3cbe482d4b22fa843608518fc
hajime-f <hajime.fujita@gmail.com>
2023-03-24 11:40:28 +0900

    株価データを更新するプログラムを完成させた

--- update.py
+++ update.py
@@ -1,7 +1,6 @@
 """
-データを初期化するためのスクリプト
+データを更新するためのスクリプト
 """
-import sys
 import urllib.request
 
 from bs4 import BeautifulSoup
@@ -32,41 +31,83 @@ def fetch_code_list():
     return code_list
 
 
-def fetch_stock_data(code):
+def fetch_stock_data_from_nikkei(code):
     """
-    銘柄コードから株価データを取得する
+    株価データを nikkei.com から取得する
     """
-    url = 'https://www.nikkei.com/nkd/company/?scode=' + str(code)
-    req = urllib.request.Request(url, method='GET')
-
+    url = f'https://www.nikkei.com/nkd/company/?scode={code}'
     try:
-        with urllib.request.urlopen(req) as res:
-            content = res.read().decode('utf-8')
-    except urllib.error.HTTPError as e:
-        sys.exit('\033[31m' + str(e) + '\033[0m')
-    except Exception as e:
-        sys.exit('\033[31m' + str(e) + '\033[0m')
+        with urllib.request.urlopen(url) as res:
+            soup = BeautifulSoup(res, 'html.parser')
+    except urllib.error.HTTPError:
+        return {}
 
-    soup = BeautifulSoup(content, 'html.parser')
+    data = {}
     values = soup.find_all("span", class_='m-stockInfo_detail_value')
 
     try:
-        open_price = float(values[0].text.translate(
+        data['open_price'] = float(values[0].text.translate(
             str.maketrans({'円': '', ',': ''})))
-        highest_price = float(values[1].text.translate(
+        data['highest_price'] = float(values[1].text.translate(
             str.maketrans({'円': '', ',': ''})))
-        lowest_price = float(values[2].text.translate(
+        data['lowest_price'] = float(values[2].text.translate(
             str.maketrans({'円': '', ',': ''})))
-        close_price = float(soup.find(
-            "dd", class_='m-stockPriceElm_value now').text.translate(str.maketrans({'円': '', ',': ''})))
-        volume = int(values[3].text.translate(
+        data['close_price'] = float(soup.find(
+            "dd", class_='m-stockPriceElm_value now').text.translate(
+                str.maketrans({'円': '', ',': ''})))
+        data['volume'] = int(values[3].text.translate(
             str.maketrans({'株': '', ',': ''})))
+        data['date'] = soup.find("div", class_='m-stockInfo_date').text
     except IndexError:
-        print(f'銘柄コード{code}のデータを取得できませんでした。')
+        data['open_price'] = -1
+        data['highest_price'] = -1
+        data['lowest_price'] = -1
+        data['close_price'] = -1
+        data['volume'] = -1
+        data['date'] = ''
 
-    date = soup.find("div", class_='m-stockInfo_date').text
+    return data
 
-    return open_price, highest_price, lowest_price, close_price, volume, date
+
+def fetch_stock_data_from_minkabu(code):
+    """
+    株価データを minkabu.jp から取得する
+    """
+    url = f'https://minkabu.jp/stock/{code}'
+    try:
+        with urllib.request.urlopen(url) as res:
+            soup = BeautifulSoup(res, 'html.parser')
+    except urllib.error.HTTPError:
+        return {}
+
+    data = {}
+    values = soup.select('div.stock_price')
+
+    try:
+        data['close_price'] = float(values[0].text.translate(
+            str.maketrans({'円': '', ',': ''})))
+    except ValueError:
+        data['close_price'] = -1
+
+    values = soup.find_all(
+        "td", class_='ly_vamd_inner ly_colsize_9_fix fwb tar wsnw')
+
+    try:
+        data['open_price'] = float(values[1].text.translate(
+            str.maketrans({'円': '', ',': ''})))
+        data['highest_price'] = float(values[2].text.translate(
+            str.maketrans({'円': '', ',': ''})))
+        data['lowest_price'] = float(values[3].text.translate(
+            str.maketrans({'円': '', ',': ''})))
+        data['volume'] = int(values[9].text.translate(
+            str.maketrans({'株': '', ',': ''})))
+    except ValueError:
+        data['open_price'] = -1
+        data['highest_price'] = -1
+        data['lowest_price'] = -1
+        data['volume'] = -1
+
+    return data
 
 
 if __name__ == '__main__':
@@ -77,10 +118,26 @@ if __name__ == '__main__':
     for code in code_list:
 
         # 株価データを取得する
-        open_price, highest_price, lowest_price, close_price, volume, date = fetch_stock_data(
-            code)
+        data_minkabu = fetch_stock_data_from_minkabu(code)
+        data_nikkei = fetch_stock_data_from_nikkei(code)
 
-        # 株価データをCSVファイルに保存する
-        with open(f'csv/{code}.csv', 'a') as f:
-            f.write(
-                f'{date}, {open_price}, {highest_price}, {lowest_price}, {close_price}, {volume}\n')
+        # データを検証する
+        if any(data_minkabu.values()) and any(data_nikkei.values()):
+
+            if data_minkabu['close_price'] == -1 or data_nikkei['close_price'] == -1:
+                continue
+            if data_minkabu['close_price'] != data_nikkei['close_price']:
+                print(f'close_price is different: {code}')
+                continue
+
+            open_price = data_nikkei['open_price']
+            highest_price = data_nikkei['highest_price']
+            lowest_price = data_nikkei['lowest_price']
+            close_price = data_nikkei['close_price']
+            volume = data_nikkei['volume']
+            date = data_nikkei['date']
+
+            # 株価データをCSVファイルに保存する
+            with open(f'csv/{code}.csv', 'a', encoding="utf-8") as f:
+                f.write(
+                    f'{date}, {open_price}, {highest_price}, {lowest_price}, {close_price}, {volume}\n')
